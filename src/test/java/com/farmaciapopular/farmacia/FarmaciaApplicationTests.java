4d8dc59cd93899984afb5c4700d5c78ddc376121
Luiz Alberto Melin <luizmelin34@gmail.com>
2022-01-25 09:14:32 -0300

    @LuizAlbertoMelin [criação do projeto]

---
+++ src/test/java/com/farmaciapopular/farmacia/FarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmaciapopular.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

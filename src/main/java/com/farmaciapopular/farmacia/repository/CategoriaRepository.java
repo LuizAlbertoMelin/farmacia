1b94854cf7ffef8e0e4031d7d0aed4488007cf0e
Luiz Alberto Melin <luizmelin34@gmail.com>
2022-01-25 09:50:53 -0300

    @LuizAlbertoMelin [criando CategoriaRepository]

---
+++ src/main/java/com/farmaciapopular/farmacia/repository/CategoriaRepository.java
@@ -0,0 +1,15 @@
+package com.farmaciapopular.farmacia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.farmaciapopular.farmacia.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+	public List<Categoria> findAllByTipoContainingIgnoreCase(String tipo);
+
+}

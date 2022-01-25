6313cc47cec3fe5bf6e689305787c834c14f4084
Daniel <kr.danielreis@gmail.com>
2022-01-25 11:08:22 -0300

    @DanielSantosReis [ADD o ProdutoRepository]

---
+++ src/main/java/com/farmaciapopular/farmacia/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.farmaciapopular.farmacia.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.farmaciapopular.farmacia.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	public List<Produto> findAllByNomeContainingIgnoreCase (String nome);
+
+}
